369025ab70a6fc12f33a62d4a3ac78d98aedd6dc
Yeon <devYeonjh@gmail.com>
2025-07-02 14:44:30 +0900

    Initialize

---
+++ Source/RuinLooters/RuinLooters.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RuinLooters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RuinLooters, "RuinLooters" );
+ 
