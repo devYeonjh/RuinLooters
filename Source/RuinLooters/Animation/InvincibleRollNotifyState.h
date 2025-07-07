b9af0ab440912d52f63432b17da09f6932ac1037
DKHan <157471675+deepsleepgood@users.noreply.github.com>
2025-07-07 12:51:59 +0900

    머지 12 37, 드래곤 공격 사망 애니메이션 추가

---
+++ Source/RuinLooters/Animation/InvincibleRollNotifyState.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "InvincibleRollNotifyState.generated.h"
+
+UCLASS()
+class RUINLOOTERS_API UInvincibleRollNotifyState : public UAnimNotifyState
+{
+    GENERATED_BODY()
+public:
+    virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration) override;
+    virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+}; 
